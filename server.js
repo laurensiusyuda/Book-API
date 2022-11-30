3ed4f7aabc4d6388c191614cba9b8b4d15203049
laurensiusyuda <yudalaurensius87@gmail.com>
2022-11-30 22:33:21 +0700

    book api

---
+++ server.js
@@ -0,0 +1,25 @@
+const Hapi = require('@hapi/hapi');
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+    // membuat request dan routing 
+    server.route([{
+        method:'GET',
+        path:'/',
+        handler: (request, h) => {
+            return'Hello World!';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About Page';
+        },
+    },]);
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+init();     
