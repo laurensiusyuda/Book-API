f76a707b44f2c4a67573e3fda63da8de82cd5d30
laurensiusyuda <yudalaurensius87@gmail.com>
2022-12-02 13:22:49 +0700

    update hander

--- src/handler.js
+++ src/handler.js
@@ -3,70 +3,78 @@ const books = require('./books');
 
 // handler untuk menambahkan data buku 
 const postNewBooks = (request, h) =>{
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading } = request.payload;
+    
+    const id = nanoid(10);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+    const finished = pageCount == readPage ? true : false;
+    
+    if (typeof name == 'undefined') {
+      const response = h.response({
+        status: 'fail',
+        message: 'Gagal menambahkan dafta buku. Mohon silahkan isi nama buku terlebih dahulu',
+      }
+      );
+      response.statusCode = 400;
+      return response;
+    }
 
-    const {name,
+    else if (readPage > pageCount) {
+      const response = h.response({
+        status: 'fail',
+        message: 'Gagal menambahkan buku. readpage lebih besar silahkan check kembali',
+      }
+      );
+      response.statusCode = 400;
+      return response;
+    }
+
+    try { const newbook = {id,
+      name,
       year,
       author,
       summary,
       publisher,
       pageCount,
       readPage,
-      reading} = request.payload;
-    
-      const id = nanoid(10);
-      const createdAt = new Date().toISOString();
-      const updatedAt = createdAt;
-      const finished = pageCount == readPage ? true : false;
-    
-      if (typeof name == 'undefined') {
-        const response = h.response({
-          status: 'fail',
-          message: 'Gagal menambahkan dafta buku. Mohon silahkan isi nama buku terlebih dahulu',
-        });
-        response.statusCode = 400;
-        return response;
-      } else if (readPage > pageCount) {
-        const response = h.response({
-          status: 'fail',
-          message: 'Gagal menambahkan buku. readpage lebih besar silahkan check kembali',
-        });
-        response.statusCode = 400;
-        return response;
-      }
+      finished,
+      reading,
+      createdAt,
+      updatedAt,
+    };
+    books.push(newbook);
 
-      try { const newbook = {id,
-        name,
-        year,
-        author,
-        summary,
-        publisher,
-        pageCount,
-        readPage,
-        finished,
-        reading,
-        createdAt,
-        updatedAt,};
-      books.push(newbook);
-      const response = h.response({
-        'status': 'success',
-        'message': 'Buku berhasil ditambahkan',
-        'data': {
-            'bookId': newbook.id,
-        },
-            },
-                );
-                response.statusCode = 201;
-                return response;
-            } 
-            catch (error) {
-                const response = h.response({
-                    status: 'error',
-                    message: 'buku gagal ditambahkan',
-            },
-                );
-                response.statusCode = 201;
-                return response;
-            };
+    const response = h.response({
+      'status': 'success',
+      'message': 'Buku berhasil ditambahkan',
+      'data': 
+      {
+        'bookId': newbook.id,
+      },
+    },
+    );
+    response.statusCode = 201;
+    return response;
+  } 
+
+  catch (error) {
+    const response = h.response({
+      status: 'error',
+      message: 'buku gagal ditambahkan',
+    },
+    );
+    response.statusCode = 201;
+    return response;
+  };
 }
 
 // handeler untuk menampilkan seluruh buku
@@ -108,6 +116,19 @@ const getBooksById = (request, h) => {
   return response;
 };
 
+// handler untuk edit buku 
+const getEditBooksById = (request, h) => {
+  const { id } = request.params;
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading} = request.payload;
+}
 module.exports = {
   postNewBooks, 
   getAllBooks, 
